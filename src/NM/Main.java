27709aa8b6446fb979a8d4b8673680bdbc01bc8c
leodann <leo_dan2010@hotmail.com>
2018-04-09 18:10:41 -0500

    Agrege la escena para punto fijo , Regla falsa, Newton Raphson y metodo de la Secante.   #Lo unico que no so ve en la tableview son los errores.

--- src/NM/Main.java
+++ src/NM/Main.java
@@ -25,16 +25,17 @@ public class Main extends Application{
     }
 
     public static void main(String[] args) {
-        launch(args);
+        //launch(args);
 
         //Bisection b1 = new Bisection(4.2,4.4,0.01,"f(x)=x^4-2*x^3-12*x^2+16*x-40");
         //ReglaFalsa r1 = new ReglaFalsa(2.8,3.3,0.01,"f(x)=x^2-2*x-3");
         //Newtton_Raphson n1 = new Newtton_Raphson(1,"f(x)=x^4-2*x^3-12*x^2+16*x-40",0.01);
-        Secante s1=new Secante(1,0,1,"f(x)=exp(-x^2)-x");
+        //Secante s1=new Secante(1,0,1,"f(x)=exp(-x^2)-x");
         //PuntoFijo P1 = new PuntoFijo(0,0.0001,"f(x)=sqrt(10/(x+4))");
 
         //Function f = new Function("f(x)=sqrt(10/(x+4))");
         //System.out.println(f.calculate(20));
 
+        Secante s1=new Secante(4.4,4.2,0.01,"f(x)=x^4-2*x^3-12*x^2+16*x-40");
     }
 }
